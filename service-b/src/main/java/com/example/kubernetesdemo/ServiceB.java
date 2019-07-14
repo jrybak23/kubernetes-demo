28273f06396cf1472f7262efaffd71429dd2c63c
jrybak2312 <jrybak2312@gmail.com>
2019-07-14 23:51:03 +0300

    initial commit

---
+++ service-b/src/main/java/com/example/kubernetesdemo/ServiceB.java
@@ -0,0 +1,13 @@
+package com.example.kubernetesdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceB {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceB.class, args);
+    }
+
+}
